0c567135064411f2957bec2d8ae1afe294b239ed
Mayukha0108 <115867152+Mayukha0108@users.noreply.github.com>
2023-08-09 13:51:11 +0530

    Update about.js

--- Proj/src/about.js
+++ Proj/src/about.js
@@ -10,7 +10,7 @@ const About = () => {
           <ProfileCard
             name="K. Vineeth"
             role="Full Stack Developer"
-            imageUrl="https://1.bp.blogspot.com/-8c7QTLoyajs/YLjr2V6KYRI/AAAAAAAACO8/ViVPQpLWVM0jGh3RZhh-Ha1-1r3Oj62wQCNcBGAsYHQ/s16000/team-1-3.jpg"
+            imageUrl="https://media.licdn.com/dms/image/C4D03AQHBOeEIw9aROQ/profile-displayphoto-shrink_800_800/0/1662521467686?e=2147483647&v=beta&t=5kEogrHxjVl8fgJCsRsHzOECgvp9YFGExBOCe_DxWyA"
             socialLinks={[
               { name: 'Facebook', iconClass: 'fab fa-facebook' },
               { name: 'Instagram', iconClass: 'fab fa-instagram' },
@@ -20,7 +20,7 @@ const About = () => {
           <ProfileCard
             name="E. Mayukha"
             role="ML Engineer"
-            imageUrl="https://1.bp.blogspot.com/-vhmWFWO2r8U/YLjr2A57toI/AAAAAAAACO4/0GBonlEZPmAiQW4uvkCTm5LvlJVd_-l_wCNcBGAsYHQ/s16000/team-1-2.jpg"
+            imageUrl="https://media.licdn.com/dms/image/C5603AQEcsGOb-z0fqw/profile-displayphoto-shrink_800_800/0/1636277855544?e=1697068800&v=beta&t=6zvAF20o1dkMTsRic4X8dmVFpMgqfFEBIS3EHYjq5p0"
             socialLinks={[
               { name: 'Facebook', iconClass: 'fab fa-facebook' },
               { name: 'Instagram', iconClass: 'fab fa-instagram' },
@@ -30,7 +30,7 @@ const About = () => {
           <ProfileCard
             name="I. Bhanu Teja"
             role="Front End Developer"
-            imageUrl="https://1.bp.blogspot.com/-AO5j2Y9lzME/YLjr3mxiqAI/AAAAAAAACPE/KAaYYTtQTrgBE3diTbxGoc4U4fCGx-C2gCNcBGAsYHQ/s16000/team-1-4.jpg"
+            imageUrl="https://media.licdn.com/dms/image/C4E03AQE5z3BZuUnvAw/profile-displayphoto-shrink_800_800/0/1626455811918?e=1697068800&v=beta&t=j-m4KYe-Xd_mJw6EKutihvU2BBCUv90FOV1lxgriXr4"
             socialLinks={[
               { name: 'Facebook', iconClass: 'fab fa-facebook' },
               { name: 'Instagram', iconClass: 'fab fa-instagram' },
